5a6c65549890fe75f20d3df3b628efb4f81684e0
Satish Yadav <monstersatish@live.com>
2021-01-06 17:11:31 +0530

    Add project files.

---
+++ AwesomeException/ExceptionExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AwesomeException
+{
+    public static class ExceptionExtensions
+    {
+        public static Stack<Exception> GetExceptionStack(this Exception exception)
+        {
+            var exceptionStack = new Stack<Exception>();
+            do
+            {
+                exceptionStack.Push(exception);
+                exception = exception.InnerException;
+            } while (exception != null);
+            return exceptionStack;
+        }
+    }
+}
