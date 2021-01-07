7b917242f54d7d912ca898a13c7018b1b1ee102d
Satish Yadav <monstersatish@live.com>
2021-01-07 09:43:55 +0530

    Package meta added. #4.

--- AwesomeException.Tests/ExceptionExtensionsTests.cs
+++ AwesomeException.Tests/ExceptionExtensionsTests.cs
@@ -22,5 +22,19 @@ namespace AwesomeException.Tests
             Assert.IsTrue(exceptionStack.Count == 4);
 
         }
+
+        [TestMethod()]
+        public void GetExceptionStack_Should_Return_Single_Exception_When_No_Inner_Exceptions_Are_Present()
+        {
+            //Arrange
+            var exception = new Exception("This is #1");
+
+            //Act
+            var exceptionStack = exception.GetExceptionStack();
+
+            //Assert
+            Assert.IsTrue(exceptionStack.Count == 1);
+
+        }
     }
 }
