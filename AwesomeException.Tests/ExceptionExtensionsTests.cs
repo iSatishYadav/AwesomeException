c08cf927a743dd86a4d5f6fb510cf12969c40f4b
Satish Yadav <monstersatish@live.com>
2021-01-07 09:26:45 +0530

    Added unit test for GetExceptionsStack. Resolves #1.

---
+++ AwesomeException.Tests/ExceptionExtensionsTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AwesomeException;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AwesomeException.Tests
+{
+    [TestClass()]
+    public class ExceptionExtensionsTests
+    {
+        [TestMethod()]
+        public void GetExceptionStack_Should_Return_All_Exceptions_When_Multiple_Inner_Exceptions_Are_Present()
+        {
+            //Arrange
+            var exception = new Exception("This is #1", new Exception("This is #2", new Exception("This is #3", new Exception("This is #4"))));
+
+            //Act
+            var exceptionStack = exception.GetExceptionStack();
+
+            //Assert
+            Assert.IsTrue(exceptionStack.Count == 4);
+
+        }
+    }
+}
